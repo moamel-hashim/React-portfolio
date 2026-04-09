0be7b60c38bd2fefec7fbf97517ee3c0e57b59a7
moamel-hashim <muamal1997@gmail.com>
2026-04-09 00:01:08 -0700

    completed the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
+import { Hero } from "@/sections/Hero";
+import { Navbar } from "@/layouts/Navbar";
+import { About } from "@/sections/About";
+import { Contact } from "@/sections/Contact";
+import { Experience } from "@/sections/Experience";
+import { Testimonials } from "@/sections/Testimonials";
 function App() {
   return <div className="min-h-screen overflow-x-hidden">
     <Navbar/>
     <main>
-
+      <Hero/>
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Testimonials/>
     </main>
   </div>
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,3 @@
-export const Navbar = () => {
+export const Hero = () => {
   return <section></section>;
 }
