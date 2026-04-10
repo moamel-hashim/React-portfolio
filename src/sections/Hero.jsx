839ee181ce01452ad530d4f2270b37466e1376b3
moamel-hashim <muamal1997@gmail.com>
2026-04-10 00:14:46 -0700

    working on my Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,42 @@
 export const Hero = () => {
-  return <section></section>;
-}
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      <div className="absolute inset-0">
+        <img
+          src="/public/images/Emerald waves on black gradient.png"
+          alt="Hero image"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-liner-to-b from-background/20 via-background/80 to-background" />
+      </div>
+
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(30)].map((_, i) => (
+          <div
+            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+            style={{
+              backgroundColor: "#20b2a6",
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animation: `slow-drift ${15 + Math.random() * 20}s ease-in-out infinite`,
+              animationDelay: `${Math.random() * 5}s`,
+            }}
+          />
+        ))};
+      </div>
+      {/*Content*/}
+      <div>
+        <div>
+          {/*left column*/}
+          <div>
+            <div>
+              <span className='w-2 h-2 bg-primary rounded-full'>Software Engineer</span>
+            </div>
+          </div>
+          {/*right column*/}
+
+        </div>
+      </div>
+    </section>
+  );
+};
